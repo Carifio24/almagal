193e33c9a6f5119d484e1c8cce0ca5e50454f472
Jonathan Carifio <carifio24@gmail.com>
2026-06-12 16:57:36 -0400

    WIP: Start working on 3D point selection.

--- src/composables/useHoverableSpreadsheetLayer.ts
+++ src/composables/useHoverableSpreadsheetLayer.ts
@@ -3,6 +3,7 @@ import { distance } from "@wwtelescope/astro";
 import { useSpreadsheetLayer, type SpreadsheetLayerOptions } from "./useSpreadsheetLayer";
 import { ImageSetType } from "@wwtelescope/engine-types";
 import { Prettify } from "@/types";
+import { Vector3d } from "@wwtelescope/engine";
 const D2R = Math.PI / 180;
 
 export interface RaDecPair {
@@ -16,10 +17,14 @@ export interface HoverableSpreadsheetLayerOptions<T extends RaDecPair> extends S
   onClick?: (row: T | null, index: number) => void;
 }
 
+
 export function useHoverableSpreadsheetLayer<T extends RaDecPair>(
   rows: T[],
   options: Prettify<HoverableSpreadsheetLayerOptions<T>> = {}
 ) {
+
+  type ClosestRowFinder = (event: PointerEvent) => { row: T, index: number } | null;
+
   const store = engineStore();
   const { pixelThreshold = 20, onHover, ...spreadsheetOptions } = options;
 
@@ -30,7 +35,7 @@ export function useHoverableSpreadsheetLayer<T extends RaDecPair>(
   const spreadsheet = useSpreadsheetLayer(points, spreadsheetOptions); // create the underlying spreadsheet layer
 
   // add mouse/pointer trackings (like green-comet, brute force)
-  function findClosestRow(event: PointerEvent) {
+  function findClosestRow2D(event: PointerEvent) {
     const pt = { x: event.offsetX, y: event.offsetY };
     const raDecDeg = store.findRADecForScreenPoint(pt);
     const targetRaRad = raDecDeg.ra * D2R;
@@ -38,7 +43,7 @@ export function useHoverableSpreadsheetLayer<T extends RaDecPair>(
 
     let minDist = Infinity;
     let closestIndex = -1;
-    
+
     // brute-force search through rows
     rows.forEach((row, i) => {
       const dist = distance(targetRaRad, targetDecRad, row.ra * D2R, row.dec * D2R);
@@ -52,18 +57,75 @@ export function useHoverableSpreadsheetLayer<T extends RaDecPair>(
 
     const closest = rows[closestIndex];
     const screenPoint = store.findScreenPointForRADec({ ra: closest.ra, dec: closest.dec });
-    
+
     // check if we are within the pixel threshold
     const pixelDist = Math.sqrt((pt.x - screenPoint.x) ** 2 + (pt.y - screenPoint.y) ** 2);
 
     return pixelDist < pixelThreshold ? { row: closest, index: closestIndex } : null;
   }
 
-  let lastResult: ReturnType<typeof findClosestRow> = null;
+  function findClosestRow3D(event: PointerEvent) {
+    const halfThreshold = Math.round(0.5 * pixelThreshold);
+    const pt = { x: event.offsetX, y: event.offsetY };
+    const columnPoints = [
+      { x: pt.x + halfThreshold, y: pt.y + halfThreshold },
+      { x: pt.x - halfThreshold, y: pt.y + halfThreshold },
+      { x: pt.x - halfThreshold, y: pt.y - halfThreshold },
+      { x: pt.x + halfThreshold, y: pt.y - halfThreshold },
+    ];
+    const rayInfo = columnPoints.map(p => store.findRayForScreenPoint(p));
+    const vertices: Vector3d[] = [];
+    rayInfo.forEach(info => {
+      vertices.push(info[0]);
+      vertices.push(Vector3d.addVectors(info[0], info[1]));
+    });
+
+    const x01 = Vector3d.subtractVectors(vertices[0], vertices[1]);
+    const x02 = Vector3d.subtractVectors(vertices[0], vertices[2]);
+    const x23 = Vector3d.subtractVectors(vertices[2], vertices[3]);
+    const x24 = Vector3d.subtractVectors(vertices[2], vertices[4]);
+    const x67 = Vector3d.subtractVectors(vertices[6], vertices[7]);
+    const x71 = Vector3d.subtractVectors(vertices[7], vertices[1]);
+    const x75 = Vector3d.subtractVectors(vertices[7], vertices[5]);
+    const normals: Vector3d[] = [
+      Vector3d.cross(x02, x24),
+      Vector3d.cross(x67, x71),
+      Vector3d.cross(x71, x75).negate(),
+      Vector3d.cross(x24, x23).negate(),
+      Vector3d.cross(x02, x01).negate(),
+      Vector3d.cross(x75, x67),
+    ];
+
+
+    const layer = spreadsheet.getLayer();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error `positions` does exist
+    const positions: Vector3d[] = layer.positions;
+
+    const indices = positions.reduce((accumulator, position, index) => {
+      if (normals.every(norm => Vector3d.dot(norm, position) < 0)) {
+        accumulator.push(index);
+      }
+      return accumulator;
+    }, [] as number[]);
+
+    if (indices.length === 0) {
+      return null;
+    } else if (indices.length === 1) {
+      const index = indices[0];
+      return { row: rows[index], index };
+    }
+
+    // If there are multiple results, we want to take the closest one
+    // As a first pass, we can find the one whose dot product with the central vector is the least
+
+  }
+
+  let lastResult: ReturnType<ClosestRowFinder> = null;
   function onPointerMove(event: PointerEvent) {
     if (store.backgroundImageset?.get_dataSetType() !== ImageSetType.sky) return; // only enable for sky layers
     if (!onHover) return;
-    const result = findClosestRow(event);
+    const result = findClosestRow2D(event);
     if (lastResult === null && result === null) return; // both null, no change
     if (result && lastResult?.index === result.index) return; // same row, no change
     onHover(result?.row ?? null, result?.index ?? -1);
@@ -73,7 +135,7 @@ export function useHoverableSpreadsheetLayer<T extends RaDecPair>(
   function onPointerDown(_event: PointerEvent) { /* i don't think we need this */ }
 
   function onPointerUp(_event: PointerEvent) { /* i don't think we need this */ }
-  
+
   function onPointerClick(event: PointerEvent) {
     if (store.backgroundImageset?.get_dataSetType() !== ImageSetType.sky) return; // only enable for sky layers
     if (!options.onClick) return;
--- src/shims-wwt.d.ts
+++ src/shims-wwt.d.ts
@@ -8,20 +8,27 @@ declare module "@wwtelescope/engine" {
     rows: string[][];
     header: string[];
   }
-  
-  
+
+
   // merges with existing SpreadSheetLayer type
   interface SpreadSheetLayer {
     get__table(): Table;
     set__table(table: Table): Table;
     dirty: boolean;
   }
-  
+
   namespace Coordinates {
     /** Returns [ra, dec] */
     function galactictoJ2000(l: number, b: number): [number, number];
     /** returns [GLON2, GLAT2] */
     function j2000toGalactic(ra: number, dec: number): [number, number]; // l is on RA, and b is on dec
-
   }
+
+  namespace Vector3d {
+    function addVectors(a: Vector3d, b: Vector3d): Vector3d;
+    function dot(a: Vector3d, b: Vector3d): number;
+    function cross(a: Vector3d, b: Vector3d): number;
+    function multiplyScalar(a: Vector3d, s: number): Vector3d;
+  }
+
 }
